f73f2cdbff6a988e294e206c68f26920ebc7fc4d
phy0M0 <162617434+phy0M0@users.noreply.github.com>
2024-04-06 18:27:48 +0630

    testing

--- resources/views/front/traditional_dance.blade.php
+++ resources/views/front/traditional_dance.blade.php
@@ -8,6 +8,7 @@
 <body>
     <section>
         <h3>This is Testing</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id perferendis mollitia praesentium quo voluptas quasi. Harum deserunt quidem illum optio, incidunt culpa, aspernatur, maiores provident deleniti ducimus obcaecati consectetur nam?</p>
     </section>
 </body>
 </html>
